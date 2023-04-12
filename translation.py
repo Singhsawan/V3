726b4cee821817f6987ba76b65a1b88fd5691a28
Nothing bro ✓ <125857863+Singhsawan@users.noreply.github.com>
2023-04-12 22:06:41 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -48,9 +48,9 @@ ABOUT_TEXT = """
     
 `📝 Language:` [Python 3](https://www.python.org/)
 `🧰 Framework:` [Pyrogram](https://github.com/pyrogram/pyrogram)
-`👨‍💻 Developer:` [Dev](t.me/ask_admin001)
-`📢 Support:` [Talk Bot](https://t.me/ask_admin001)
-`🌐 Source Code:` [GitHub](https://github.com/kevinnadar22/URL-Shortener-V2/)
+`👨‍💻 Developer:` [Dev](t.me/J_shree_ram)
+`📢 Support:` [Talk Bot](https://t.me/bot_channel_011)
+`🌐 Source Code:` [DM ME](https://t.me/J_shree_ram/)
 """
 
 
