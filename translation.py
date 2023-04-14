b7603d5ef580c7ff605f194c872a76b52061bd21
Nothing bro ✓ <125857863+Singhsawan@users.noreply.github.com>
2023-04-14 13:21:41 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -48,7 +48,7 @@ ABOUT_TEXT = """
     
 📝 Language: [Python 3](https://www.python.org/)
 🧰 Framework: [Pyrogram](https://github.com/pyrogram/pyrogram)
-👨‍💻 Developer: [Dev](t.me/ask_admin001)
-📢 Support: [Talk Bot](https://t.me/t2linkspromotion_bot)
-🌐 Source Code: [GitHub](https://github.com/T2links)
+👨‍💻 Developer: [Dev](t.me/J_shree_ram)
+📢 Support: [Talk Bot](https://t.me/bot_channel_011)
+🌐 Source Code: [DM ME](https://t.me/J_shree_ram)
 """
